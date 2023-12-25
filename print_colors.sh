27ff577e512130f1506d795e7c84d82aaf307604
Seungho Lee <lxxseunghh@gmail.com>
2023-12-25 15:27:23 +0900

    Change BG color block height to 10

--- print_colors.sh
+++ print_colors.sh
@@ -19,7 +19,7 @@ echo "\n"
 # print background colors
 text="         "
 bgs=(40 41 42 43 44 45 46 47)
-for i in {1..5}; do
+for i in {1..10}; do
     for bg in $bgs[@]; do
         echo -n " "
         echo -en " \033[${fg}m\033[${bg}m${text}\033[0m "
